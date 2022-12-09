d6cadb1e4e3cd6580cf6aed25cc3e035a9a58a57
cardandres <65802352+cardandres@users.noreply.github.com>
2022-12-09 16:15:58 -0500

    Add error message for invalid place variable

--- main.py
+++ main.py
@@ -10,21 +10,24 @@ option = st.selectbox("Select data to view: ", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
 if place:
-    filtered_data = get_data(place, days)
+    try:
+        filtered_data = get_data(place, days)
 
-    if option == 'Temperature':
-        temperatures = [dict['main']['temp'] for dict in filtered_data]
-        dates = [dict['dt_txt'] for dict in filtered_data]
-        figure = px.line(x=dates, y=temperatures, 
-                        labels={"x": "Date", "y": "Temperature (C)"})
+        if option == 'Temperature':
+            temperatures = [dict['main']['temp'] for dict in filtered_data]
+            dates = [dict['dt_txt'] for dict in filtered_data]
+            figure = px.line(x=dates, y=temperatures, 
+                            labels={"x": "Date", "y": "Temperature (C)"})
 
-        st.plotly_chart(figure)
-    if option == 'Sky':
-        images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png", 
-                    "Rain": "images/rain.png", "Snow": "images/snow.png"}
-        sky_conditions = [dict['weather'][0]['main'] for dict in filtered_data]
+            st.plotly_chart(figure)
+        if option == 'Sky':
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png", 
+                        "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            sky_conditions = [dict['weather'][0]['main'] for dict in filtered_data]
 
-        # List comprehension
-        image_paths = [images[condition] for condition in sky_conditions]
+            # List comprehension
+            image_paths = [images[condition] for condition in sky_conditions]
 
-        st.image(image_paths, width=117)
+            st.image(image_paths, width=117)
+    except KeyError:
+        st.error("Place entered not found.")
